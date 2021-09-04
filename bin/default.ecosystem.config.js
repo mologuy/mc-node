ff2f0c10476524cac9c8675c94511fbfb63ad4e1
mologuy <mike@mologuy.com>
2021-09-04 03:21:39 +0000

    changed default type

--- bin/default.ecosystem.config.js
+++ bin/default.ecosystem.config.js
@@ -3,8 +3,13 @@ module.exports = {
     name   : "mc-server",
     script : "./app.js",
     version: "1.0.0",
+    treekill: false,
+    wait_ready: true,
+    listen_timeout: 30000,
+    kill_timeout: 6000,
     env: {
-      MC_TIMEOUT: 5000
+      "STOP_TIMEOUT": 5000,
+      "MAX_HEAP_SIZE":  3000,
     }
   }]
 }
