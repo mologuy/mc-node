c9210bf71ae0352fe0344dfdff6ffd37e51059d6
mologuy <mike@mologuy.com>
2021-09-04 23:54:18 +0000

    moved download function to main file

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const readline = require("readline");
 const child = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
 
 const mcStopTimeoutMS = process.env.MC_STOP_TIMEOUT || 5000;
 const serverName = process.env.MC_SERVER_FILENAME || "server.jar";
 const mcMaxHeapSize = process.env.MAX_HEAP_SIZE || 1024;
 const mcInitialHeapSize = process.env.INIT_HEAP_SIZE || 1024;
+const mcVersion = process.env.MC_VERSION || "latest";
+
+const serverPath = path.join(__dirname,"server-files");
+const serverFilePath = path.join(serverPath, serverName);
 
 /**
  * @type {readline.Interface}
@@ -40,7 +47,6 @@ async function mcExitCallback(code) {
 async function sigintCallback() {
     rl_interface.close();
     mc.stdin.write("stop\n");
-    process.send("ready");
     mcTimeout = setTimeout(()=>{
         console.log(`Server didn't stop in ${mcStopTimeoutMS/1000} seconds, forcing stop...`);
         mc.kill("SIGKILL");
@@ -58,11 +64,73 @@ async function readyCallback(data) {
     }
 }
 
+async function downloadServer(){
+    return new Promise(async (resolve, reject)=>{
+        const versionManifestUrl = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
+        /**
+         * @type {axios.AxiosResponse}
+         */
+        let res = await axios({method: "get", url: versionManifestUrl, responseType: "json"});
+
+        let versionToDownload;
+        if (mcVersion == "latest") {
+            versionToDownload = res.data.latest.release;
+        }
+        else {
+            versionToDownload = mcVersion;
+        }
+
+        const downloadVersionManifestUrl = res.data.versions.find((entry) => entry.id == versionToDownload).url;
+        res = await axios({method: "get", url: downloadVersionManifestUrl, responseType: "json"});
+
+        const downloadServerUrl = res.data.downloads.server.url;
+        console.log(`Downloading server version ${versionToDownload} from:`, downloadServerUrl);
+
+        const tempfilepath = path.join(serverPath, "download.temp");
+        const finalfilepath = serverFilePath;
+
+        const writer = fs.createWriteStream(tempfilepath);
+        res = await axios({method:"get", url: downloadServerUrl, responseType: "stream"});
+        res.data.pipe(writer);
+    
+        writer.on("finish", async ()=>{
+            await fs.promises.rename(tempfilepath, finalfilepath);
+            console.log("Download done.");
+            resolve();
+        });
+
+        writer.on("error", reject);
+    });
+}
+
+async function checkForDownload() {
+    if (!fs.existsSync(serverPath)) {
+        await fs.promises.mkdir(serverPath);
+    }
+    try {
+        await fs.promises.access(serverFilePath, fs.constants.F_OK | fs.constants.W_OK)
+        console.log(`${serverName} found`);
+        return;
+    }
+    catch (e) {
+        if (e.code == "ENOENT") {
+            console.log("Server not found, downloading...");
+            await downloadServer();
+            return;
+        }
+        else {
+            throw e;
+        }
+    }
+}
+
 async function main() {
+    await checkForDownload();
+
     rl_interface = readline.createInterface({input: process.stdin, output: process.stdout});
     rl_interface.on("line", stdinCallback);
 
-    mc = child.spawn("java", [`-Xmx${mcMaxHeapSize}M`, `-Xmx${mcInitialHeapSize}M`, "-jar", serverName, "nogui"],{detached: true, cwd: "./server-files"});
+    mc = child.spawn("java", [`-Xmx${mcMaxHeapSize}M`, `-Xmx${mcInitialHeapSize}M`, "-jar", serverName, "nogui"],{detached: true, cwd: serverPath});
     mc.stdout.pipe(process.stdout);
     mc.on("exit", mcExitCallback);
     mc.stdout.on("data", readyCallback);
--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,32 +1,11 @@
 const fs = require("fs");
-const axios = require("axios");
 const path = require("path");
 let options;
 
-const serverpath = path.join(__dirname, "..", "server-files");
-
-async function downloadServer(){
-    const versionManifestUrl = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
-    let res = await axios({method: "get", url: versionManifestUrl, responseType: "json"});
-    const latestVersion = res.data.latest.release;
-    const latestManifestUrl = res.data.versions.find((entry) => entry.id == latestVersion).url;
-    res = await axios({method: "get", url: latestManifestUrl});
-    const latestServerUrl = res.data.downloads.server.url;
-    console.log("Downloading from:", latestServerUrl);
-
-    const writer = fs.createWriteStream(path.join(serverpath, "temp.file"));
-    res = await axios({method:"get", url: latestServerUrl, responseType: "stream"});
-    res.data.pipe(writer);
-
-    writer.on("finish", async ()=>{
-        await fs.promises.rename(path.join(serverpath, "temp.file"), path.join(serverpath, "server.jar"));
-        console.log("Done.")
-    });
-}
+const optionspath = path.join(__dirname, "..", "config.json");
+const defaultoptionspath = path.join(__dirname, "default.config.json");
 
 async function loadOptionFile() {
-    const optionspath = path.join(__dirname, "..", "config.json");
-    const defaultoptionspath = path.join(__dirname, "default.config.json");;
     if (fs.existsSync(optionspath)) {
         options = require(optionspath);
         console.log("Loaded config.json");
@@ -41,20 +20,4 @@ async function loadOptionFile() {
 //main
 (async ()=>{
     await loadOptionFile();
-    if (!fs.existsSync(serverpath)) {
-        await fs.promises.mkdir(serverpath);
-    }
-    try {
-        await fs.promises.access(path.join(serverpath, "server.jar"), fs.constants.F_OK | fs.constants.W_OK)
-        console.log(`server.jar found`);
-    }
-    catch (e) {
-        if (e.code == "ENOENT") {
-            console.log("Server not found, downloading...");
-            await downloadServer();
-        }
-        else {
-            throw e;
-        }
-    }
 })();
