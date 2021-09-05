825250fed71660c078ee9aa41a43bb727361b356
mologuy <mike@mologuy.com>
2021-09-05 23:14:35 +0000

    added chmod for script files

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const child_process = require("child_process");
 let options;
 
 const optionspath = path.join(__dirname, "..", "config.json");
@@ -17,7 +18,37 @@ async function loadOptionFile() {
     }
 }
 
+/**
+ * @param {string} command 
+ * @returns {Promise<string>}
+ */
+function execPromise(command) {
+    return new Promise((resolve, reject)=>{
+        child_process.exec(command, (err, stdout, stderr)=>{
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(stdout);
+            }
+        })
+    });
+}
+
+async function chmodScripts() {
+    const shellScriptsDir = path.join(__dirname, "..", "shell_scripts");
+    const backupScript = path.join(shellScriptsDir, "backup.sh");
+    const updateScript = path.join(shellScriptsDir, "update.sh");
+    await execPromise(`chmod ug+x ${backupScript}`);
+    await execPromise(`chmod ug+x ${updateScript}`);
+}
+
 //main
 (async ()=>{
+    chmodScripts()
+    .catch((e)=>{
+        console.log("Couldn't add execute permissions to shell scripts.", e);
+    });
+
     await loadOptionFile();
 })();
